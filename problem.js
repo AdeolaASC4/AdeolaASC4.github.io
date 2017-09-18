ab68bd266220a3d7604d04f83159d8d26111d4b7
Adeola Ebunlomo <adeolaebunlomo@gmail.com>
2017-09-17 20:07:53 -0400

    removed unwanted file

--- problem.js
+++
@@ -1,7 +0,0 @@
-var numbers = [1, 5, 10, 15];
-var doubles = numbers.map(function(x) {
-   return x * 2;
-});
-console.log(doubles)
-// doubles is now [2, 10, 20, 30]
-// numbers is still [1, 5, 10, 15]
